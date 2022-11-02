97caf95d5c3e586e0f5c8f759d43347d11d05df1
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-02 12:49:30 -0300

    update hide button if you are in that route in web navbar

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -1,23 +1,35 @@
 import React from "react";
 import './web.css';
+import { useLocation } from 'react-router-dom'
 
 function Web() {
+  const { pathname } = useLocation()
   return <div className="web">
-    <div className="web-option">
-        <a href="projects" >
-          <i className="fi-rr-edit-alt option-icon"></i>Projects
-        </a>
-    </div>
-    <div className="web-option">
-        <a href="skills">
-        <i className="fi-rr-laptop option-icon"></i>Skills
-        </a>
-    </div>
-    <div className="web-option">
-        <a href="work">
-        <i className="fi-rr-briefcase option-icon"></i>Work
-        </a>
-    </div>
+    {
+      pathname === '/projects'?null:
+      <div className="web-option">
+      <a href="projects" >
+        <i className="fi-rr-edit-alt option-icon"></i>Projects
+      </a>
+     </div>
+    }
+    {
+      pathname === '/skills'?null:
+      <div className="web-option">
+      <a href="skills">
+      <i className="fi-rr-laptop option-icon"></i>Skills
+      </a>
+      </div>
+    }
+    {
+      pathname === '/work'?null:
+      <div className="web-option">
+      <a href="work">
+      <i className="fi-rr-briefcase option-icon"></i>Work
+      </a>
+      </div>
+    }
+
   </div>;
 }
 
