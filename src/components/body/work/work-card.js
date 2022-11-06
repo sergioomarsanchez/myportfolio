749dbfce674eacea2acb09dae23978d4b3bdb64a
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-06 15:47:06 -0300

    Light version work cards

--- src/components/body/work/work-card.js
+++ src/components/body/work/work-card.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./work-card.css";
+import { ThemeContext } from '../../../contexts/theme'
 function WorkCard ({ item }) {
+  const [isDark] = useContext(ThemeContext)
   return (
     <div key={item.company} className="work-card">
       <img src={item.companyLogo}className="work-logo" alt={item.company}/>
       <div className="work-info">
         <label className="company-name">{item.company}</label>
-        <div className="work-dates">
+        <div className={isDark?"work-dates":"work-datesLight"}>
           <label>{item.dateJoining}</label>-<label>{item.dateEnd}</label>
         </div>
         <div className="work-desc">
