0a2fc218d345a7ac4cdc4c386fcd84d0f776348d
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-07 19:06:50 -0300

    button back to top added

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -1,13 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./contact.css";
 import Separator from "../../common/separator";
 import SocialContact from "../../common/social-contact";
 import { ThemeContext } from "../../../contexts/theme";
 function Contact() {
   const [isDark] = useContext(ThemeContext)
+  const [isVisible, setIsVisible] = useState(0)
+
+  const toggleVisibility = ()=>{
+    if(window.scrollY > 300){
+      setIsVisible(100)
+    } else {
+      setIsVisible(0)
+    }
+  }
+
+  const scrollToTop = ()=>{
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    })
+  }
+
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+    return () =>{
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+  
+
   return (
     <div className="contact">
     <Separator />
+    <button id="button" onClick={scrollToTop} style={{opacity: isVisible}} class="noselect"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg></button>
     <label className={isDark?"section-title":"section-titleLight"}>Contact</label>
     <div className="contact-container">
       <div className="contact-left">
