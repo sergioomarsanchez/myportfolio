965b255167376bbcd385033425f1bc1d50b65f65
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-16 11:42:46 -0300

    innecesary console.log removed

--- src/components/body/work/work-card.js
+++ src/components/body/work/work-card.js
@@ -1,10 +1,7 @@
 import React, { useContext } from "react";
 import "./work-card.css";
 import { ThemeContext } from '../../../contexts/theme'
-const  API_KEY = process.env.REACT_APP_API_KEY
-console.log(API_KEY)
-const credenciales = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${API_KEY}`
-console.log(credenciales)
+
 
 function WorkCard ({ item }) {
   const [isDark] = useContext(ThemeContext)
