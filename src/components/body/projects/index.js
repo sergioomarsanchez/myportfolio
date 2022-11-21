a2d19e5d83de293d322b4a4ed625d2f2745257b1
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-21 12:52:13 -0300

    projects  img added

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -4,13 +4,21 @@ import Separator from "../../common/separator";
 import { ProjectData } from "../../data/projects";
 import ProjectCard from "./project-card";
 import "./projects.css";
+import img from '../../../assets/img/The Little Things - Working Windows.png'
+import imgScreen from '../../../assets/img/Amigos - Screen.png'
 function Projects() {
   const data = ProjectData;
   const [isDark] = useContext(ThemeContext)
   return (
     <div className="projects">
       <Separator />
+      <div className="title-img">
       <label className={isDark?"section-title":"section-titleLight"}>Projects</label>
+      <div className="projects-imgContainer">
+        <img className={isDark? 'projectImgScreen': 'projectImgScreenLight'} src={imgScreen} alt=''/>  
+        <img className={isDark? 'projectImgWindows': 'projectImgWindowsLight'} src={img} alt=''/>
+      </div>
+      </div>
       <div className="projects-containers">
         {data.map((project) => {
           return <ProjectCard project={project} />;
