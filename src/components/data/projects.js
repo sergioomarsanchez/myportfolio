69cb7586dc8d8f74d602c09ec7b0fba72cd98d00
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-01-05 13:12:28 -0300

    pizza project links updated

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -29,9 +29,9 @@ export const ProjectData=[
         title:"Pizza Delivery App",
         about: "This is a Pizza delivery app. As a client you can select several pizzas, add them to the cart, and proceed to confirm the payment with PayPal or on delivery, once confirm you can check the order detail to follow the progress of it. As an admin, you can add and delete products, and update order status. Authentication is made with the cookies system.",
         tags:["Nextjs", "ReactJS", "Javascript", "MongoDB", "Html/Css", "Redux", "Javascript", "Mongoose", "Cloudinary"],
-        github:"https://github.com/sergioomarsanchez/PI-Foods",
+        github:"https://github.com/sergioomarsanchez/pizzaapp",
         image:pizzaapp,
-        demo:"https://github.com/sergioomarsanchez#:~:text=CSS-,pizzaapp,-Public",
+        demo:"https://pizzaapp-tau.vercel.app/",
         videoLink:"https://youtu.be/kkzPIZAvYB0"
     },
 ];
