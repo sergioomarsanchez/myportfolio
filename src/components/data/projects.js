32495ad2e849bc7df9fcf5b078ef14a85081c3c9
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-01-05 13:03:51 -0300

    pizza project added

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,5 +1,6 @@
 import moviefy from '../../assets/Moviefy.png'
 import thefoodapp from '../../assets/thefoodapp.png'
+import pizzaapp from '../../assets/pizzaApp.png'
 
 
 export const ProjectData=[
@@ -17,10 +18,20 @@ export const ProjectData=[
         id:2,
         title:"The Food App",
         about: "This is an individual project for my Bootcamp at Soy Henry. Is an App to check Recipes, search, order by name and heath rating and filter by recipe type, and you can also create your own recipes. Please, click on the project image to check a short demo video",
-        tags:["ReactJS", "Javascript", "PostgreSQL", "Html/Css", "Redux", "Javascript", "express", "external API"],
+        tags:["ReactJS", "Javascript", "PostgreSQL", "Html/Css", "Redux", "Express", "external API"],
         github:"https://github.com/sergioomarsanchez/PI-Foods",
         image:thefoodapp,
         demo:"https://pi-thefoodap-sergioosanchez.netlify.app/",
         videoLink:"https://youtu.be/uqTnidI7LRc"
+    },
+    {
+        id:3,
+        title:"Pizza Delivery App",
+        about: "Hello Network, I would like to share my latest project. It is a Pizza delivery app. As a client you can select several pizzas, add them to the cart, and proceed to confirm the payment with PayPal or on delivery, once confirm you can check the order detail to follow the progress of it. As an admin, you can add and delete products, and update order status. Authentication is made with the cookies system.",
+        tags:["Nextjs", "ReactJS", "Javascript", "MongoDB", "Html/Css", "Redux", "Javascript", "Mongoose", "Cloudinary"],
+        github:"https://github.com/sergioomarsanchez/PI-Foods",
+        image:pizzaapp,
+        demo:"https://github.com/sergioomarsanchez#:~:text=CSS-,pizzaapp,-Public",
+        videoLink:"https://youtu.be/kkzPIZAvYB0"
     },
 ];
