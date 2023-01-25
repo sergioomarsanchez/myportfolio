e634ff1142b0b3797002b2ed0b6e3abbacc2271e
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-01-25 13:26:57 -0300

    About descr updated

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -21,9 +21,7 @@ function About() {
         <div className="div-for-mobile">
         <div className="about-text">
           <h3>About me...</h3>
-          <p>I started off my career in IT many years ago studying Systems Analysis in the IES Siglo XXI college.
-             In the same institution I got a bachelor's degree in Busines Management and now I've completed my IT education with the Full Stack Web Developer course at Henry.
-             I've worked in Purchasing/Sales where I developed skills as team player and a customer focused mindset.</p>
+          <p>I started off my career in IT many years ago studying Systems Analysis at the IES Siglo XXI college. In the same institution, I got a bachelor's degree in Busines Management and now I've completed my IT education with the Full Stack Web Developer course at Henry. I've worked in Purchasing/Sales where I developed skills as a team player and a customer-focused mindset.</p>
         </div>
         <div className="about-photo">
         <Dog/>
