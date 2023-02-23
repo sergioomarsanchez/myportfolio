f9da6f65af8b224483d83a627ea81aae9b69de6b
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-02-23 11:22:32 -0300

    change from 2022 to 2023

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">Sergio Sánchez, 2022</div>;
+  return <div className="footer">Sergio Sánchez, 2023</div>;
 }
 
 export default Footer;
