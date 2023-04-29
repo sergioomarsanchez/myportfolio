9734b949a5341b6d3dc2e9905c94f2abb5618129
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-04-29 12:46:26 -0300

    navbar links light styles and display updated

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -10,17 +10,17 @@ function Web() {
   return <div className="web">
       <Link to={'/projects'} style={{ textDecoration: 'none'}}>
       <div className={isDark?"web-option":"web-optionLight"} >
-        <i className="fi-rr-edit-alt option-icon" style={pathname === '/projects'?{transform: "scale(1.1)", color: "#82bfc2" }:null }>Projects</i>
+        <i className="fi-rr-edit-alt option-icon" style={pathname === '/projects'?{transform: "scale(1.1)", color: isDark? "#82bfc2":"#3f0a0a" }:null }>Projects</i>
       </div>
       </Link>
       <Link to={'/skills'} style={{ textDecoration: 'none' }}>
       <div className={isDark?"web-option":"web-optionLight"}>
-      <i className="fi-rr-laptop option-icon" style={pathname === '/skills'?{transform: "scale(1.1)", color: "#82bfc2" }:null }>Skills</i>
+      <i className="fi-rr-laptop option-icon" style={pathname === '/skills'?{transform: "scale(1.1)", color: isDark? "#82bfc2":"#3f0a0a" }:null }>Skills</i>
       </div>
       </Link>
       <Link to={'/work'} style={{ textDecoration: 'none' }}>
       <div className={isDark?"web-option":"web-optionLight"}>
-      <i className="fi-rr-briefcase option-icon" style={pathname === '/work'?{transform: "scale(1.1)", color: "#82bfc2" }:null }>Work</i>
+      <i className="fi-rr-briefcase option-icon" style={pathname === '/work'?{transform: "scale(1.1)", color: isDark? "#82bfc2":"#3f0a0a" }:null }>Work</i>
       </div>
       </Link>
   </div>;
