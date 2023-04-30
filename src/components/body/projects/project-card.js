ee5be881e0110d106889be6e4b1d4f287795ba03
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-04-30 12:41:55 -0300

    projects width corrected, navbar with in 820px corrected

--- src/components/body/projects/project-card.js
+++ src/components/body/projects/project-card.js
@@ -1,47 +1,79 @@
 import React, { useContext } from "react";
 import "./project-card.css";
-import { ThemeContext } from '../../../contexts/theme'
-
+import { ThemeContext } from "../../../contexts/theme";
 
 function ProjectCard({ project }) {
+  const [isDark] = useContext(ThemeContext);
 
-    const [isDark] = useContext(ThemeContext)
-  
   return (
     <div key={project.title} className="project-card">
       <div className="project-info">
         <label className="project-title">{project.title}</label>
         <div className="project-links">
           {project.demo && (
-            <a className="project-link" href={project.demo} target='_blank' rel="noreferrer">
-              <div className={isDark?"link-button":"link-buttonLight"}>
+            <a
+              className="project-link"
+              href={project.demo}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className={isDark ? "link-button" : "link-buttonLight"}>
                 <i className="fi-rr-globe"> Demo</i>
               </div>
             </a>
-            )}
-            {project.github&& (
-              <a className="project-link" href={project.github} target='_blank' rel="noreferrer">
-                <div className={isDark?"link-button":"link-buttonLight"}>
+          )}
+          {project.github && (
+            <a
+              className="project-link"
+              href={project.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className={isDark ? "link-button" : "link-buttonLight"}>
                 <i className="devicon-github-original"> Github</i>
               </div>
             </a>
-            )}
+          )}
         </div>
         <p>{project.about}</p>
         <div className="project-tags">
-          {project.tags.map((tag, index)=> {
-            return <label key={tag+index} className={isDark?"tag":"tagLight"}>{tag}</label>;
+          {project.tags.map((tag, index) => {
+            return (
+              <label key={tag + index} className={isDark ? "tag" : "tagLight"}>
+                {tag}
+              </label>
+            );
           })}
         </div>
       </div>
-       
-      {
-        project.image && (<a className="project-ytlink"  href={project.videoLink} target='_blank' rel="noreferrer"> <img className={isDark?"project-photo":"project-photoLight"} src={project.image}  alt={project.image} /> </a>)
-      }
-      <div className={isDark?"tiltBottom":"tiltBottomLight"}>
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className={isDark? "shape-fill":"shape-fillLight"}></path>
-          </svg>
+
+      {project.image && (
+        <a
+          className="project-ytlink"
+          href={project.videoLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <img
+            className={isDark ? "project-photo" : "project-photoLight"}
+            src={project.image}
+            alt={project.image}
+          />{" "}
+        </a>
+      )}
+      <div className={isDark ? "tiltBottom" : "tiltBottomLight"}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            className={isDark ? "shape-fill" : "shape-fillLight"}
+          ></path>
+        </svg>
       </div>
     </div>
   );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,46 +2,55 @@ import React, { useContext, useState } from "react";
 import "./header.css";
 import Mobile from "./mobile";
 import Web from "./web/index";
-import { useLocation, Link } from 'react-router-dom'
-import logo from '../../assets/logo/logo_white_large.png'
-import logoLight from '../../assets/logo/logo_small.png'
+import { useLocation, Link } from "react-router-dom";
+import logo from "../../assets/logo/logo_white_large.png";
+import logoLight from "../../assets/logo/logo_small.png";
 import { ThemeContext } from "../../contexts/theme";
 import ToggleButton from "../common/toggleButton/toggleButton";
 
-
-
 function Header() {
-  const [isOpen, setIsOpen]=useState(false);
-  const { pathname } = useLocation()
-  const [isDark] = useContext(ThemeContext)
+  const [isOpen, setIsOpen] = useState(false);
+  const { pathname } = useLocation();
+  const [isDark] = useContext(ThemeContext);
 
   return (
-    <div className={isDark?"header":"headerLight"}>
-              {
-      pathname === '/'? <div className="logo">
-      { isDark? <img src={logo} alt='logo'className="logo"/>:<img src={logoLight} alt='logo'className="logo"/>
-      }
-    </div>:
-      <Link to={'/'} style={{ textDecoration: 'none' }}>
+    <div className={isDark ? "header" : "headerLight"}>
+      {pathname === "/" ? (
         <div className="logo">
-          { isDark? <img src={logo} alt='logo'className="logo"/>:<img src={logoLight} alt='logo'className="logo"/>
-          }
+          {isDark ? (
+            <img src={logo} alt="logo" className="logo" />
+          ) : (
+            <img src={logoLight} alt="logo" className="logo" />
+          )}
         </div>
-    </Link>
-    }
-      <div className={isDark?"menu":"menuLight"}>
+      ) : (
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <div className="logo">
+            {isDark ? (
+              <img src={logo} alt="logo" className="logo" />
+            ) : (
+              <img src={logoLight} alt="logo" className="logo" />
+            )}
+          </div>
+        </Link>
+      )}
+      <div className={isDark ? "menu" : "menuLight"}>
         <div className="web-menu">
           <Web />
         </div>
         <div className="mobile-menu">
-          <div onClick={()=>setIsOpen(!isOpen)}>
-            <i className={isDark?"fi-rr-apps menu-icon":"fi-rr-apps menu-iconLight"}></i>
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <i
+              className={
+                isDark ? "fi-rr-apps menu-icon" : "fi-rr-apps menu-iconLight"
+              }
+            ></i>
           </div>
           {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
         </div>
       </div>
-      <div className='toggleContainer'>
-        <ToggleButton/>
+      <div className="toggleContainer">
+        <ToggleButton />
       </div>
     </div>
   );
