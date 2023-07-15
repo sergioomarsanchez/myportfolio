c6fc2825183b71bbae54eb6333564f9edf7ccc22
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-07-15 13:20:38 -0300

    eliminate contact title

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -36,7 +36,6 @@ function Contact() {
     <div className="contact">
     <Separator />
     <button id={isDark?"button":"buttonLight"} onClick={scrollToTop} style={{opacity: isVisible}} className={isDark?"noselect":"noselectLight"}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg> </button>
-    <label className={isDark?"section-title":"section-titleLight"}>Contact</label>
     <div className="contact-container">
       <div className="contact-left">
         <p>Want to get in touch? Contact me on any of these platforms</p>
