c8965afa1afb25e3fd5980084af3c739a9496a54
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-07-15 11:54:49 -0300

    new separator design, images references links update.

--- src/components/data/references.js
+++ src/components/data/references.js
@@ -5,7 +5,7 @@ export const ReferencesData = [
     title:"Curator of People | Brewer of Coffee | Enabling Qualitative Human Insights @ Sonar",
     workRelation: 'Teamate',
     linkedinProfile: "https://www.linkedin.com/in/tanjida-rahman-151815122/",
-    image: 'https://media.licdn.com/dms/image/C5603AQGZ7lgd6Y3GqQ/profile-displayphoto-shrink_100_100/0/1655303413132?e=1689206400&v=beta&t=zH8TrtuADiOkLNZlKWslUgOGzIscF7AMQTv-7NauDPA',
+    image: 'https://media.licdn.com/dms/image/C5603AQGZ7lgd6Y3GqQ/profile-displayphoto-shrink_200_200/0/1655303413132?e=1694649600&v=beta&t=0xg6aVquLOXxo95Nq0RvRy9lNkt4mD2-ffP0oaHM_xs',
     referenceText: "Sergio is a fantastic colleague, helpful and insightful. During our shared time at YourWatches, he was the one who did my onboarding into the company. He is reliable and well versed in excel and other office suite programs. He is great at sales and customer service because he can connect with customers very naturally. He has great work ethic and always strives to do his best. As a person, he is warm and kind. Any company and/ or organization would benefit from having him on their team."
   },
   { 
@@ -14,7 +14,7 @@ export const ReferencesData = [
     title:"RRHH",
     workRelation: 'Teamate',
     linkedinProfile: "https://www.linkedin.com/in/marielacarrizo/",
-    image: 'https://media.licdn.com/dms/image/C4E03AQHQ2z5zP1PR9A/profile-displayphoto-shrink_100_100/0/1559995805511?e=1689206400&v=beta&t=l2EbQLC8brHpkb4wPIAmfj3k3cg7gH1RuU9wXDVUCz4',
+    image: 'https://media.licdn.com/dms/image/C4E03AQHQ2z5zP1PR9A/profile-displayphoto-shrink_200_200/0/1559995805511?e=1694649600&v=beta&t=eKfeZj1iMIThQC4m9UfOKwoKKle0ml8nZi1NozGTIg8',
     referenceText: "Sergio has been my successor in the purchasing position at Ormay, demonstrating attention, willingness, and commitment to learning and developing in the role. I would recommend Sergio for any job that requires analytical skills, organization, and responsibility. He is a friendly and kind person who facilitates a positive work environment."
   },
   { 
@@ -23,7 +23,7 @@ export const ReferencesData = [
     title:"Cost specialist, Bachelor of Business Administration",
     workRelation: 'Manager',
     linkedinProfile: "https://www.linkedin.com/in/alvaro-gonzalez-21161518/",
-    image: 'https://media.licdn.com/dms/image/C4D03AQGLdcP0ZqX0Bw/profile-displayphoto-shrink_100_100/0/1651511572185?e=1689206400&v=beta&t=thlyCv-dktMOfoO6_Yekoxp4MeCRLIDE5_mMZhZpktg',
+    image: 'https://media.licdn.com/dms/image/C4D03AQGLdcP0ZqX0Bw/profile-displayphoto-shrink_200_200/0/1651511572185?e=1694649600&v=beta&t=j5qotKpynvIohRmIkpyR5venghKU1jWkq_nM2UBWy6A',
     referenceText: "Since he joined the team, he was committed to our tasks. He learned details quickly and improved processes. He recognized important aspects and tracked his duties in a proactive way. He has forged strong interpersonal relationships and has a great sense of humor. I am lucky to be able to share time with him. His professionalism and willingness to improve set him apart as a great coworker."
   },
   { 
@@ -32,7 +32,7 @@ export const ReferencesData = [
     title:"Purchasing agent",
     workRelation: 'Teamate',
     linkedinProfile: "https://www.linkedin.com/in/crinejo-carlos-2b248531/",
-    image: 'https://media.licdn.com/dms/image/C4E03AQExgyXajQP_qQ/profile-displayphoto-shrink_100_100/0/1518885924470?e=1689206400&v=beta&t=jEhdVavZ6bD2HHH3g4_VhCq59oTh82cMtdubw09iK2Y',
+    image: 'https://media.licdn.com/dms/image/C4E03AQExgyXajQP_qQ/profile-displayphoto-shrink_200_200/0/1518885924470?e=1694649600&v=beta&t=l0N0TUJkbQ2qnvQ9WDgciYrh-AmKJDCHE9Sai_AJwi8',
     referenceText: "I worked with Sergio at Systel He is an excellent professional who is willing to work in a team and continuously contributes new ideas to the group. With a great personality and sense of humor, he enhances his ability to work, making all issues flow without any inconvenience. He provided countless economic improvements by sourcing components from new suppliers abroad thanks to his continuous search for them. His goal was always to improve himself as a professional, which resulted in highly positive actions for the company. I hope to have Sergio as a colleague again sometime in the future!"
   },
   { 
@@ -41,7 +41,7 @@ export const ReferencesData = [
     title:"Master of supply chain en ENEB - Escuela de Negocios Europea de Barcelona",
     workRelation: 'Manager',
     linkedinProfile: "https://www.linkedin.com/in/marcelo-ramon-gonzalez-09682b52/",
-    image: 'https://media.licdn.com/dms/image/C4D03AQE4kKFJ_J1XZg/profile-displayphoto-shrink_100_100/0/1630732364893?e=1689206400&v=beta&t=GnINfPvBCk-sSs4jEQ00Nn9Qy8s-Lgt9OQ_gidZ9FKU',
+    image: 'https://media.licdn.com/dms/image/C4D03AQE4kKFJ_J1XZg/profile-displayphoto-shrink_200_200/0/1630732364893?e=1694649600&v=beta&t=o7mTLvKgAZoSs7YBZ2FK4A6dk7JOOh2EAJlACpu3CK4',
     referenceText: "I have worked with Sergio since 2016 and he has constantly impressed me with his ability to work in a team and his experience in the purchasing area. He has achieved all the goals in the area, especially in reducing costs and improving the efficiency of our supply chain. In addition, he has performed functions in foreign trade, an area where he has extensive knowledge. Sergio is an excellent professional and even better person. He is highly recommended, and I would always like to have people like him on my team."
   }
 ];
--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,70 +1,72 @@
-import homifyLogo from '../../assets/icons/logo-homify-2.png'
-import ywLogo from '../../assets/icons/yourwatches.logo.png'
-import systelLogo from '../../assets/icons/systel.logo.png'
-import tend from '../../assets/icons/trabajo-en-digital.png'
-import logo from '../../assets/logo/logo_small_icon_only.png'
+import homifyLogo from "../../assets/icons/logo-homify-2.png";
+import ywLogo from "../../assets/icons/yourwatches.logo.png";
+import systelLogo from "../../assets/icons/systel.logo.png";
+import tend from "../../assets/icons/trabajo-en-digital.png";
+import logo from "../../assets/logo/logo_small_icon_only.png";
 export const WorkData = [
-    {
-        company: "Front-End Developer at Trabajo en Digital" ,
-        designation: "Front-End Developer",
-        dateJoining: "05-2023",
-        dateEnd: "Date",
-        companyLogo:tend,
-        work:"- Developed the front-end of a job board page, using technologies such as Next.js, Tailwind, and jQuery to achieve an attractive and functional user interface. Made significant contributions to the project's code by implementing innovative solutions and improving development efficiency.",
-        location: "México-Remote"
-    },
-    {
-        company: "Full Stack Developer Freelancer at SOS Sergio Omar Sánchez" ,
-        designation: "Full Stack Developer",
-        dateJoining: "11-2022",
-        dateEnd: "04-2023",
-        companyLogo:logo,
-        work:"- Specializing in designing and implementing complex web applications for my clients. Adept at working independently as a freelancer or as part of a team in a remote environment. Lead full-stack developer with expertise in Next.js, MongoDB, JWT, API, Node.js, and React.js Specializing in designing and implementing complex web applications for my clients Passionate about staying up-to-date with the latest trends and technologies in web development.",
-        location: "Córdoba-Argentina"
-    },
-    {
-        company: "Full-Stack Teaching Assistant at Soy Henry" ,
-        designation: "Full-Stack Teaching Assistant",
-        dateJoining: "06-2022",
-        dateEnd: "10-2022",
-        companyLogo:"https://s3.amazonaws.com/tracxn-data-image/logo/company/X3jhC-Yp_400x400_a2f3eadb-d8f7-4fb5-af86-2cb6967202ef.jpg",
-        work:"Coordinate a group of students to achieve integration into the study group. Guide students in the first steps of the course. Assist to solve exercises and promote group collaboration (Pair Programming). Propose ideas to improve the Bootcamp processes.",
-        location: "Córdoba-Argentina"
-    },
-    {
-        company: "Full Stack Developer Bootcamp at Henry" ,
-        designation: "Student",
-        dateJoining: "06.2022",
-        dateEnd: "10.2022",
-        companyLogo:"https://s3.amazonaws.com/tracxn-data-image/logo/company/X3jhC-Yp_400x400_a2f3eadb-d8f7-4fb5-af86-2cb6967202ef.jpg",
-        work:"- Skills: Nodejs, Sequelize, Postgres, Express, Javascript, HTML ,CSS, Less, React, Redux, Git, Github, Jest and Supertest",
-        location: "Córdoba-Argentina"
-    },
-    {
-        company: "Sales Executive at Homify" ,
-        designation: "Sales Executive",
-        dateJoining: "10.2021",
-        dateEnd: "02.2022",
-        companyLogo:homifyLogo,
-        work:"-Helped our clients to improve their webpage SEO by giving them advice.-Based on the investigation of their webpage and marketing of their company I contacted potential clients to the platform.",
-        location: "Berlin-Germany"
-    },
-    {
-        company: "Sales/Purchasing agent at Yourwatches.com" ,
-        designation: "Sales/Purchasing",
-        dateJoining: "09-2020",
-        dateEnd: "06-2021",
-        companyLogo:ywLogo,
-        work:"Development of new suppliers and merchants, revision of quotation, negotiation with suppliers (Europe, Americas), experience on Chronos 24 platform for Customer Support and Sales. Shipments tracking and follow up and training of new profiles for the Sales and Purchase teams.",
-        location: "Copenhagen-Denmark"
-    },
-    {
-        company: "International Purchasing agent at Systel.SA" ,
-        designation: "Purchasing",
-        dateJoining: "07-2017",
-        dateEnd: "02-2020",
-        companyLogo:systelLogo,
-        work:"- Revision of quotation, negotiation with suppliers (United States, Italy, China, Brazil), stock control by MRP (Materials Requirement Planning), shipments tracking and follow up and development of new suppliers",
-        location: "Córdoba-Argentina"
-    }
-]
+  {
+    company: "Front-End Developer at Trabajo en Digital",
+    designation: "Front-End Developer",
+    dateJoining: "05-2023",
+    dateEnd: "Date",
+    companyLogo: tend,
+    work: "- Developed the front-end of a job board page, using technologies such as Next.js, Tailwind, and jQuery to achieve an attractive and functional user interface. Made significant contributions to the project's code by implementing innovative solutions and improving development efficiency.",
+    location: "México-Remote",
+  },
+  {
+    company: "Full Stack Developer Freelancer at SOS Sergio Omar Sánchez",
+    designation: "Full Stack Developer",
+    dateJoining: "11-2022",
+    dateEnd: "04-2023",
+    companyLogo: logo,
+    work: "- Specializing in designing and implementing complex web applications for my clients. Adept at working independently as a freelancer or as part of a team in a remote environment. Lead full-stack developer with expertise in Next.js, MongoDB, JWT, API, Node.js, and React.js Specializing in designing and implementing complex web applications for my clients Passionate about staying up-to-date with the latest trends and technologies in web development.",
+    location: "Córdoba-Argentina",
+  },
+  {
+    company: "Full-Stack Teaching Assistant at Soy Henry",
+    designation: "Full-Stack Teaching Assistant",
+    dateJoining: "06-2022",
+    dateEnd: "10-2022",
+    companyLogo:
+      "https://s3.amazonaws.com/tracxn-data-image/logo/company/X3jhC-Yp_400x400_a2f3eadb-d8f7-4fb5-af86-2cb6967202ef.jpg",
+    work: "Coordinate a group of students to achieve integration into the study group. Guide students in the first steps of the course. Assist to solve exercises and promote group collaboration (Pair Programming). Propose ideas to improve the Bootcamp processes.",
+    location: "Córdoba-Argentina",
+  },
+  {
+    company: "Full Stack Developer Bootcamp at Henry",
+    designation: "Student",
+    dateJoining: "06.2022",
+    dateEnd: "10.2022",
+    companyLogo:
+      "https://s3.amazonaws.com/tracxn-data-image/logo/company/X3jhC-Yp_400x400_a2f3eadb-d8f7-4fb5-af86-2cb6967202ef.jpg",
+    work: "- Skills: Nodejs, Sequelize, Postgres, Express, Javascript, HTML ,CSS, Less, React, Redux, Git, Github, Jest and Supertest",
+    location: "Córdoba-Argentina",
+  },
+  {
+    company: "Sales Executive at Homify",
+    designation: "Sales Executive",
+    dateJoining: "10.2021",
+    dateEnd: "02.2022",
+    companyLogo: homifyLogo,
+    work: "-Helped our clients to improve their webpage SEO by giving them advice.-Based on the investigation of their webpage and marketing of their company I contacted potential clients to the platform.",
+    location: "Berlin-Germany",
+  },
+  {
+    company: "Sales/Purchasing agent at Yourwatches.com",
+    designation: "Sales/Purchasing",
+    dateJoining: "09-2020",
+    dateEnd: "06-2021",
+    companyLogo: ywLogo,
+    work: "Development of new suppliers and merchants, revision of quotation, negotiation with suppliers (Europe, Americas), experience on Chronos 24 platform for Customer Support and Sales. Shipments tracking and follow up and training of new profiles for the Sales and Purchase teams.",
+    location: "Copenhagen-Denmark",
+  },
+  {
+    company: "International Purchasing agent at Systel.SA",
+    designation: "Purchasing",
+    dateJoining: "07-2017",
+    dateEnd: "02-2020",
+    companyLogo: systelLogo,
+    work: "- Revision of quotation, negotiation with suppliers (United States, Italy, China, Brazil), stock control by MRP (Materials Requirement Planning), shipments tracking and follow up and development of new suppliers",
+    location: "Córdoba-Argentina",
+  },
+];
