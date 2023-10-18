2a8c6020f29438763b39ec4ef5a72a4347670a36
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-10-18 16:03:17 -0300

    update back to top colors and display button

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -4,45 +4,56 @@ import Separator from "../../common/separator";
 import SocialContact from "../../common/social-contact";
 import { ThemeContext } from "../../../contexts/theme";
 function Contact() {
-  const [isDark] = useContext(ThemeContext)
-  const [isVisible, setIsVisible] = useState(0)
+  const [isDark] = useContext(ThemeContext);
+  const [isVisible, setIsVisible] = useState(0);
 
-  const toggleVisibility = ()=>{
-    if(window.scrollY > 300){
-      setIsVisible(100)
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(100);
     } else {
-      setIsVisible(0)
+      setIsVisible(0);
     }
-  }
+  };
 
-  const scrollToTop = ()=>{
+  const scrollToTop = () => {
     window.scrollTo({
-      top:0,
-      behavior:"smooth"
-    })
-  }
-
-
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', toggleVisibility)
-    return () =>{
-      window.removeEventListener('scroll', toggleVisibility)
-    }
-  }, [])
-  
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
 
   return (
     <div className="contact">
-    <Separator />
-    <button id={isDark?"button":"buttonLight"} onClick={scrollToTop} style={{opacity: isVisible}} className={isDark?"noselect":"noselectLight"}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg> </button>
-    <div className="contact-container">
-      <div className="contact-left">
-        <p>Want to get in touch? Contact me on any of these platforms</p>
-        <SocialContact />
+      <Separator />
+      <button
+        id={isDark ? "button" : "buttonLight"}
+        onClick={scrollToTop}
+        style={{ opacity: isVisible }}
+        className={isDark ? "noselect" : "noselectLight"}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z" />
+        </svg>{" "}
+      </button>
+      <div className="contact-container">
+        <div className="contact-left">
+          <p>Want to get in touch? Contact me on any of these platforms</p>
+          <SocialContact />
         </div>
       </div>
-    </div>  
+    </div>
   );
 }
 
